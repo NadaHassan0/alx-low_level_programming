f819681672484b176a15d15c1de138c49175a3c6
NadaHassan0 <hnada7678@gmail.com>
2023-07-13 13:08:47 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Description: This function prints the string 
- *              using the puts function.
+ * Description: A C program that prints with put function
  *
  * Return: Always 0 (success)
  */
-int main (void)
-{ puts ("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+int main (void)
+{
+puts  ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,8 +7,9 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
-{ printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
 
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
