f725f15b3e5f452d1912e4c81da7bcb9702b484c
NadaHassan0 <hnada7678@gmail.com>
2023-07-24 10:51:26 -0700

    c programming

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,21 @@
 #include"main.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+
+void rev_string(char *s)
+{
+	size_t len = strlen(s);
+
+	for (size_t i = 0; i < len / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
